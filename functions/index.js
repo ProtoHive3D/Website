60c7cbc7a50f85544be483b3851e20628c8ee5ab
ProtoHive3D <protohiveenterprises@gmail.com>
2025-08-20 15:55:02 -0500

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,8 @@
+export async function onRequest() {
+  const html = await fetch("https://protohive3d.com/home.html");
+  const response = new Response(await html.text(), html);
+  response.headers.set("X-Content-Type-Options", "nosniff");
+  response.headers.set("X-Frame-Options", "DENY");
+  response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+  return response;
+}
