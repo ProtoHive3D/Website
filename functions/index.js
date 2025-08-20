3d719d71faf4b4a89dee9911f5055dcef60c2c62
ProtoHive3D <protohiveenterprises@gmail.com>
2025-08-20 15:49:32 -0500

    Delete functions/index.js

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest({ request }) {
-  const html = await fetch(request);
-  const response = new Response(await html.text(), html);
-  response.headers.set("X-Content-Type-Options", "nosniff");
-  response.headers.set("X-Frame-Options", "DENY");
-  response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-  return response;
-}
