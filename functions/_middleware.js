a69f618fa88b47d3975e96c3e5917aaa5e8c6f76
ProtoHive3D <protohiveenterprises@gmail.com>
2025-08-20 16:05:35 -0500

    Delete functions/_middleware.js

--- functions/_middleware.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest({ request, next }) {
-  const response = await next();
-  response.headers.set("X-Content-Type-Options", "nosniff");
-  response.headers.set("X-Frame-Options", "DENY");
-  response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-  return response;
-}
